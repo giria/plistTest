3ec76a7c1205398e722fcb037f0294e1b162dbb1
giria <joan@giria.com>
2015-03-17 20:13:43 +0100

    insertar datos

--- TestPlist/ViewController.m
+++ TestPlist/ViewController.m
@@ -14,6 +14,69 @@
 
 @implementation ViewController
 
+- (IBAction)readPlist:(UIButton *)sender {
+    
+    NSString* plistPath = [[NSBundle mainBundle] pathForResource:@"Mercadona" ofType:@"plist"];
+    NSDictionary * contentDictionary = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+    
+    NSLog(@" Dictionary %@",contentDictionary);
+    
+    
+    NSArray *listaCompra =contentDictionary[@"items"];
+    
+    NSLog(@" Arrays %@",listaCompra);
+
+    
+    
+    
+    
+    
+    
+}
+
+
+
+- (IBAction)saveToPlist:(UIButton *)sender {
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *path = [documentsDirectory stringByAppendingPathComponent:@"Mercadona33.plist"];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSLog(@" directorio %@", path);
+    
+//    if (![fileManager fileExistsAtPath: path])
+//    {
+//        path = [documentsDirectory stringByAppendingPathComponent: [NSString stringWithFormat: @"Mercadona33.plist"] ];
+//    }
+
+    NSMutableDictionary *data;
+    
+    if ([fileManager fileExistsAtPath: path])
+    {
+        data = [[NSMutableDictionary alloc] initWithContentsOfFile: path];
+    }
+    else
+    {
+        // If the file doesn’t exist, create an empty dictionary
+        data = [[NSMutableDictionary alloc] init];
+    }
+    
+    //To insert the data into the plist
+    int value = 5;
+    [data setObject:[NSNumber numberWithInt:value] forKey:@"value"];
+    [data writeToFile: path atomically:YES];
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ }
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
